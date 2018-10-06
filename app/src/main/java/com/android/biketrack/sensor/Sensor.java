61d07b0cfaa346718257e54193f11cc71d848958
Gionata Boccalini <gionata.boccalini@gmail.com>
2018-10-06 19:23:19 +0200

    Added first dummy implementation of SensorManager.

--- app/src/main/java/com/android/biketrack/sensor/Sensor.java
+++ app/src/main/java/com/android/biketrack/sensor/Sensor.java
@@ -1,9 +1,21 @@
 package com.android.biketrack.sensor;
 
 public final class Sensor {
-  private Sensor() {}
+    private Sensor() {
+    }
 
-  public static final class SensorDataSet {
-  }
+    public enum SensorState {
+        NONE,
+        CONNECTING,
+        CONNECTED,
+        DISCONNECTED,
+        SENDING;
+    }
 
-}
+    public static final class SensorDataSet {
+        private boolean hasCreationTime;
+        private long creationTime_ = 0L;
+        public boolean hasCreationTime() { return hasCreationTime; }
+        public long getCreationTime() { return creationTime_; }
+    }
+    }
